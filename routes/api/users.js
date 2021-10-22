90d404469dbbf6e04ad292b47361a9a2ab0eab0a
OO7kartik <rohit.audrey@gmail.com>
2021-10-22 22:58:06 +0530

    Add middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -54,17 +56,26 @@ router.post(
 
       await user.save();
 
-      // return jsonwebtoken
-      res.send('User register');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server error');
     }
-
-    res.send(req.body);
   }
 );
 
 module.exports = router;
-
-// contri check
