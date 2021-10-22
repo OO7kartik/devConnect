4b8e97e6b2f59e335374d7d5eb0acaad39f44138
OO7kartik <rohiit.audrey@gmail.com>
2021-10-22 16:03:00 +0530

    Add check for existing user, avatar, bcrypt to users.js

--- config/db.js
+++ config/db.js
@@ -2,19 +2,18 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoUri');
 
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true, 
+      useNewUrlParser: true,
     });
 
     console.log('mongodb connected...');
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
 
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register route
@@ -15,11 +19,48 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // return jsonwebtoken
+      res.send('User register');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+
     res.send(req.body);
   }
 );
