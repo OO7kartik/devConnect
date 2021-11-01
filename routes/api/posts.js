68580231d44a58809cfc78e3ab21731277c825d1
OO7kartik <rohit.audrey@gmail.com>
2021-11-01 16:41:04 +0530

    Add route to delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -199,4 +199,40 @@ router.post(
   }
 );
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    delete comment from a post
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).send('Comment does not exists');
+    }
+
+    // check if user made the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).send('User not authorized');
+    }
+
+    // remove comment
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
