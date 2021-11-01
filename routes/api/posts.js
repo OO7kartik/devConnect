019bcce8226efc0096739fc962ca740429ae6732
OO7kartik <rohit.audrey@gmail.com>
2021-11-01 16:27:48 +0530

    Add route to make a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const User = require('../../models/User');
 
 // @route   POST api/posts
 // @desc    create a post
-// @access  Public
+// @access  Private
 router.post(
   '/',
   [auth, [check('text', 'text is required').not().isEmpty()]],
@@ -163,5 +163,40 @@ router.put('/unlike/:id', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/posts/comment/:id
+// @desc    comments on post with id = :id
+// @access  Private
+router.post(
+  '/comment/:id',
+  [auth, [check('text', 'text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
